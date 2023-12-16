26ebe30c46fe1bbc270ef3bd4dddd4f205908e43
toptalentdev001 <nguyencuonglinh.star426@gmail.com>
2023-12-16 02:07:19 +0100

    Remove reels div from Youtube search results

--- WebScraping/YoutubeScraper.cs
+++ WebScraping/YoutubeScraper.cs
@@ -152,6 +152,13 @@ namespace WebScraping
             // Most recent search results
             //driver.Navigate().GoToUrl($"https:www.youtube.com/results?search_query={youtubeSearchTerm}&sp=CAI%253D");
 
+            // Find the reels element to remove
+            IWebElement reelsDiv = driver.FindElement(By.CssSelector("ytd-reel-shelf-renderer.style-scope.ytd-item-section-renderer"));
+
+            // Execute JavaScript to remove the div element from the DOM
+            IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)driver;
+            jsExecutor.ExecuteScript("arguments[0].parentNode.removeChild(arguments[0]);", reelsDiv);
+
             var videoTitles = GetVideoTitles(driver);
             var videoViews = GetVideoViews(driver);
             var videoAuthors = GetVideoAuthors(driver);
